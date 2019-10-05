0cabba74013a3db7e5cf6ec124e12f742f262d5a
Usman Khalid <usmankhalid2591@users.noreply.github.com>
2019-10-05 17:04:37 +0500

    Code

---
+++ Way of earning/js/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.left = "0";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.left = "-300px";
+  }
+  
+  
